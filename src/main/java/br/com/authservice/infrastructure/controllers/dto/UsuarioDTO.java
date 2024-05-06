a91e802158ebbf04f0614de9e2aa078325c24a74
Emanuel Brito <emanuelguimaraes23@gmail.com>
2024-05-06 17:23:01 -0300

    Implementação do serviço 'RegistrarUsuario' de ponta a ponta, juntamente com seus testes unitários

---
+++ src/main/java/br/com/authservice/infrastructure/controllers/dto/UsuarioDTO.java
@@ -0,0 +1,30 @@
+package br.com.authservice.presentation.controllers.dto;
+
+import br.com.authservice.core.domain.model.Permissao;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    @NotNull(message = "Permissões são obrigatórias")
+    private List<Permissao> permissoes;
+}
